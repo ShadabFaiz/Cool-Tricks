edaf21a30ba4b0fa0387838b04d9cc66b2214bfd
ShadabFaiz <shadabfaiz607@gmail.com>
2018-10-07 09:36:27 +0530

    Added 2 different animation on different section

--- Scroll Animation/script/scroll.js
+++ Scroll Animation/script/scroll.js
@@ -19,6 +19,36 @@
 //     // }
 // }
 // For Multiple Elements
+// const lowerBound = -20;
+// const upperBound = 10;
+// const viewport = document.documentElement;
+// const childrenElements = document.getElementsByTagName("section");
+// let childElement;
+// let VisibleElements: {} = {};
+// window.onscroll = (ev: any) => {
+//     for (const key in childrenElements) {
+//         if (isHTMLElement(childrenElements[key])) {
+//             childElement = childrenElements[key];
+//             animate(childElement.children[0]);
+//         }
+//     }
+// }
+// const isHTMLElement = (element) => {
+//     return typeof element === 'object' ? true : false;
+// }
+// const animate = (el: HTMLElement) => {
+//     console.log(el);
+//     let positionFromTop = (el.getBoundingClientRect().top / viewport.clientHeight) * 100;
+//     if (positionFromTop < 40 && positionFromTop > lowerBound) {
+//         el.classList.replace('hide', 'top');
+//         VisibleElements[el.id] = 'showing';
+//     }
+//     //uncomment these if you want the element to hide if we move away from them.
+//     // else {
+//     //     el.classList.replace('top', 'hide');
+//     //     delete VisibleElements[el.id];
+//     // }
+// }
 var lowerBound = -20;
 var upperBound = 10;
 var viewport = document.documentElement;
@@ -26,21 +56,32 @@ var childrenElements = document.getElementsByTagName("section");
 var childElement;
 var VisibleElements = {};
 window.onscroll = function (ev) {
-    for (var key in childrenElements) {
-        if (isHTMLElement(childrenElements[key])) {
-            childElement = childrenElements[key];
-            animate(childElement.children[0]);
-        }
-    }
+    // for (const key in childrenElements) {
+    //     if (isHTMLElement(childrenElements[key])) {
+    //         childElement = childrenElements[key];
+    //             animateOnChildren(childElement);
+    //     }
+    // }
+    Array.from(childrenElements).forEach(function (el, index) {
+        console.log(index + " \n " + el);
+        if (index === 0)
+            animateOnParent(el);
+        else
+            animateOnChildren(el);
+    });
 };
 var isHTMLElement = function (element) {
     return typeof element === 'object' ? true : false;
 };
-var animate = function (el) {
-    console.log(el);
+/************************* Animation 2 **************************
+ * All the Child Element will be visible at the same time.
+ * *************************************************************/
+var animateOnParent = function (el) {
     var positionFromTop = (el.getBoundingClientRect().top / viewport.clientHeight) * 100;
     if (positionFromTop < 40 && positionFromTop > lowerBound) {
-        el.classList.replace('hide', 'top');
+        Array.from(el.children).forEach(function (element) {
+            element.classList.replace('hide', 'top');
+        });
         VisibleElements[el.id] = 'showing';
     }
     //uncomment these if you want the element to hide if we move away from them.
@@ -48,4 +89,21 @@ var animate = function (el) {
     //     el.classList.replace('top', 'hide');
     //     delete VisibleElements[el.id];
     // }
+};
+/*********************** Animation 2 *********************************
+ Each Element will be visible at different time based uopon its position.
+**********************************************************************/
+var animateOnChildren = function (parent) {
+    Array.from(parent.children).forEach(function (el) {
+        var positionFromTop = (el.getBoundingClientRect().top / viewport.clientHeight) * 100;
+        if (positionFromTop < 40 && positionFromTop > lowerBound) {
+            el.classList.replace('hide', 'top');
+            VisibleElements[el.id] = 'showing';
+        }
+        //uncomment these if you want the element to hide if we move away from them.
+        // else {
+        //     el.classList.replace('top', 'hide');
+        //     delete VisibleElements[el.id];
+        // }
+    });
 };
